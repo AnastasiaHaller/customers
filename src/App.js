503ad2b54dc5cbd20737e87dc548f6a06cacf782
Anastasia Haller <anadana87@gmail.com>
2023-08-25 11:07:15 +0200

    changed to class componenets, added filter, used context

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import Customers from "./components/Customers";
+import CustomerFilter from "./components/CustomerFilter";
+import CustomersContext from "./store/customers-context";
+
+const DUMMY_CUSTOMERS = [
+  { id: "c1", name: "John" },
+  { id: "c2", name: "Amanda" },
+  { id: "c3", name: "Edith" },
+];
 
 function App() {
+  const customersContext = {
+    customers: DUMMY_CUSTOMERS,
+  };
+
   return (
-    <div>
-      <Customers />
-    </div>
+    <CustomersContext.Provider value={customersContext}>
+      <CustomerFilter />
+    </CustomersContext.Provider>
   );
 }
 
